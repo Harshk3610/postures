67cc200da2f6a276c15df9e8a17fa6cd2cdca48c
Harshk3610 <117203974+Harshk3610@users.noreply.github.com>
2024-05-23 13:57:01 +0530

    Update human_posture_analysis_video.py

--- human_posture_analysis_video.py
+++ human_posture_analysis_video.py
@@ -4,8 +4,7 @@ import math as m
 import mediapipe as mp
 #import pygame
 
-# Initialize pygame mixer
-pygame.mixer.init()
+# Initialize pygame mixer pygame.mixer.init()
 
 # Calculate distance
 def findDistance(x1, y1, x2, y2):
@@ -20,8 +19,8 @@ def findAngle(x1, y1, x2, y2):
 
 # Function to send alert.
 #def sendWarning():
-    pygame.mixer.music.load('mixkit-classic-short-alarm-993.wav')
-    pygame.mixer.music.play()
+    #pygame.mixer.music.load('mixkit-classic-short-alarm-993.wav')
+    #pygame.mixer.music.play()
 
 # Constants and Initializations
 good_frames = 0
@@ -109,6 +108,6 @@ def process_frame(image, fps):
         cv2.putText(image, time_string_bad, (10, h - 20), font, 0.9, red, 2)
 
     #if bad_time > 3:
-        sendWarning()
+        #sendWarning()
 
     return image
